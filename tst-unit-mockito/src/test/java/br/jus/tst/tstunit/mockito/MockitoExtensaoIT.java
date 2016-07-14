acb7e792403860b1d02cd3bedf74343812418dab
Thiago Miranda <C053102@tst.jus.br>
2016-07-14 16:13:41 -0300

    Adicionando teste de integração da extensão Mockito.

---
+++ tst-unit-mockito/src/test/java/br/jus/tst/tstunit/mockito/MockitoExtensaoIT.java
@@ -0,0 +1,30 @@
+package br.jus.tst.tstunit.mockito;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import br.jus.tst.tstunit.TstUnitRunner;
+
+/**
+ * Testes de integração da {@link MockitoExtensao}.
+ * 
+ * @author Thiago Miranda
+ * @since 14 de jul de 2016
+ */
+@RunWith(TstUnitRunner.class)
+@HabilitarMockito
+public class MockitoExtensaoIT {
+
+    @Mock
+    private Object mock;
+
+    @Test
+    public void deveriaCarregarMocks() {
+        assertThat(mock, is(notNullValue(Object.class)));
+    }
+}
