ee46ac159c7ac520bdee7511bdc0e98698dc4650
Thiago Miranda <C053102@tst.jus.br>
2019-03-28 19:35:51 -0300

    [CI] Ajustes no script de deploy

--- ci/before-deploy.sh
+++ ci/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_653259e5b3a8_key -iv $encrypted_653259e5b3a8_iv -in codesigning.asc.enc -out codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_653259e5b3a8_key -iv $encrypted_653259e5b3a8_iv -in ci/codesigning.asc.enc -out ci/codesigning.asc -d
     gpg --fast-import ci/codesigning.asc
 fi
